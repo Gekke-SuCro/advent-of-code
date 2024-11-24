aac5aa42525fad382572c95ef97714c4af0569fe
Gekke-SuCro <jayden.roeper@student.hu>
2024-11-24 01:16:47 +0100

    GearRatios now tracks all numbers in the engine schematic.

--- src/main/java/com/jaydenroeper/adventOfCode2023/day03/GearRatios.java
+++ src/main/java/com/jaydenroeper/adventOfCode2023/day03/GearRatios.java
@@ -2,7 +2,9 @@ package com.jaydenroeper.adventOfCode2023.day03;
 
 import com.jaydenroeper.adventOfCode2023.utils.FileUtils;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class GearRatios {
     private static final String specialChars = "/*!@#$%^&*()\"{}_[]|\\?/<>,";
@@ -17,7 +19,54 @@ public class GearRatios {
         loadEngineSchematic(fileName);
     }
 
-    public void loadEngineSchematic(String engineSchematic) {
+    public int getSumOfPartNumbers() {
+        int sum = 0;
+        List<String> adjacentPartNumbers = findAdjacentPartNumbers();
+        for (String partNumber : adjacentPartNumbers) {
+            sum += Integer.parseInt(partNumber);
+        }
+
+        System.out.println(adjacentPartNumbers);
+
+        return sum;
+    }
+
+    private List<String> findAdjacentPartNumbers() {
+        List<String> adjacentPartNumbers = new ArrayList<>();
+
+        for (int i = 0; i < gearMatrix.length; i++) {
+            StringBuilder number = new StringBuilder();
+            boolean wasDigit = false;
+            boolean isAdjacent = false;
+
+            for (int j = 0; j < gearMatrix[i].length; j++) {
+                String part = gearMatrix[i][j];
+                boolean isDigit = Character.isDigit(part.charAt(0));
+                if (!isAdjacent) {
+                    isAdjacent = isAdjacent(part, i, j);
+                }
+
+                if (isDigit) {
+                    number.append(part);
+                    wasDigit = true;
+                } else {
+                    if (wasDigit && isAdjacent) {
+                        adjacentPartNumbers.add(number.toString());
+                        number = new StringBuilder();
+                    }
+                    wasDigit = false;
+                }
+            }
+        }
+
+        return adjacentPartNumbers;
+    }
+
+    private boolean isAdjacent(String digit, int row, int col) {
+        return true;
+    }
+
+    private void loadEngineSchematic(String engineSchematic) {
         String[] lines = engineSchematic.split("\n");
         gearMatrix = new String[lines.length][lines[0].trim().split("").length];
 
@@ -37,6 +86,8 @@ public class GearRatios {
         String engineSchematic = FileUtils.readFileToString("day03/example.txt");
 
         GearRatios gearRatios = new GearRatios(engineSchematic);
+        int sum = gearRatios.getSumOfPartNumbers();
 
+        System.out.println("Sum of Part Numbers: " + sum);
     }
 }
