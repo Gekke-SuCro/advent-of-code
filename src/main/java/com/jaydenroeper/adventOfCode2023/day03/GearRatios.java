967982281df212ad07b284556e8fc14427c4868d
Gekke-SuCro <jayden.roeper@student.hu>
2024-11-24 01:05:53 +0100

    GearRatios now loads engine schematice string to a string matri ([][]).

--- src/main/java/com/jaydenroeper/adventOfCode2023/day03/GearRatios.java
+++ src/main/java/com/jaydenroeper/adventOfCode2023/day03/GearRatios.java
@@ -2,13 +2,12 @@ package com.jaydenroeper.adventOfCode2023.day03;
 
 import com.jaydenroeper.adventOfCode2023.utils.FileUtils;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Arrays;
 
 public class GearRatios {
-    private String [][] gearMatrix;
+    private static final String specialChars = "/*!@#$%^&*()\"{}_[]|\\?/<>,";
 
-    private String specialChars = "/*!@#$%^&*()\"{}_[]|\\?/<>,";
+    private String [][] gearMatrix;
 
     public GearRatios() {
 
@@ -18,140 +17,26 @@ public class GearRatios {
         loadEngineSchematic(fileName);
     }
 
-    public void loadEngineSchematic(String fileName) {
-        String engineString = FileUtils.readFileToString(fileName);
-        String[] lines = engineString.trim().split("\n");
-        int width = lines[0].trim().split("").length;
-        int height = lines.length;
+    public void loadEngineSchematic(String engineSchematic) {
+        String[] lines = engineSchematic.split("\n");
+        gearMatrix = new String[lines.length][lines[0].trim().split("").length];
 
-        gearMatrix = new String [width][height];
-
-        int x = 0;
-        int y = 0;
+        int row = 0;
         for (String line : lines) {
-            for (String c : line.trim().split("")) {
-//                System.out.println("X: " + x + " Y: " + y);
-                gearMatrix[y][x] = c.trim();
-                x++;
+            int col = 0;
+            for (String character : line.trim().split("")) {
+                gearMatrix[row][col] = character.trim();
+                col++;
             }
-            x = 0;
-            y++;
+            row++;
         }
     }
 
-    public int getSumOfPartNumbers() {
-        int sum = 0;
-
-        List<String> numbers = new ArrayList<>();
-        List<String> adjacentNumbers = new ArrayList<>();
-
-        for (int i = 0; i < gearMatrix.length; i++) {
-            StringBuilder number = new StringBuilder();
-            boolean isAdjacent = false;
-            boolean wasNumber = false;
-
-            for (int j = 0; j < gearMatrix[i].length; j++) {
-                String s = gearMatrix[i][j];
-                char c = s.charAt(0);
-//                System.out.println(c + " digit:");
-                if (Character.isDigit(c)) {
-                    number.append(s);
-//                    System.out.println("digit: " + c);
-                    isAdjacent = isAdjacentToSymbol(s, i, j);
-                    if (isAdjacent) {
-                        System.out.println("isAdjacent: " + number);
-                    }
-
-                    wasNumber = true;
-                } else {
-                    if (wasNumber) {
-                        if (isAdjacent) {
-                            adjacentNumbers.add(number.toString());
-                        }
-                        numbers.add(number.toString());
-                        number = new StringBuilder();
-                    }
-
-                    wasNumber = false;
-                    isAdjacent = false;
-                }
-            }
-        }
-
-        System.out.println(numbers);
-        System.out.println(adjacentNumbers);
-
-        return sum;
-    }
-
-    private boolean isAdjacentToSymbol(String digit, int row, int col) {
-        System.out.println("Checking if adjacent: " + digit + " " + row + " " + col);
-
-        boolean isXSide = (col == 0 || col == gearMatrix[0].length - 1);
-        boolean isYSide = (row == 0 || row == gearMatrix.length - 1);
-//        System.out.println("IsXSide: " + isXSide + " IsYSide: " + isYSide);
-
-        StringBuilder adjacentChars = new StringBuilder();
-
-
-        try {
-            String charTopLeft = gearMatrix[row - 1][col - 1];
-            adjacentChars.append(charTopLeft);
-        } catch (Exception _) {
-        }
-        try {
-            String charTop = gearMatrix[row - 1][col];
-            adjacentChars.append(charTop);
-        } catch (Exception _) {
-        }
-        try {
-            String charTopRight = gearMatrix[row - 1][col + 1];
-            adjacentChars.append(charTopRight);
-        } catch (Exception _) {
-        }
-
-        try {
-            String charLeft = gearMatrix[row][col - 1];
-            adjacentChars.append(charLeft);
-        } catch (Exception _) {
-        }
-        try {
-            String charRight = gearMatrix[row][col + 1];
-            adjacentChars.append(charRight);
-        } catch (Exception _) {
-        }
-
-        try {
-            String charBottomLeft = gearMatrix[row + 1][col - 1];
-            adjacentChars.append(charBottomLeft);
-        } catch (Exception _) {
-        }
-        try {
-            String charBottom = gearMatrix[row + 1][col];
-            adjacentChars.append(charBottom);
-        } catch (Exception _) {
-        }
-        try {
-            String charBottomRight = gearMatrix[row + 1][col + 1];
-            adjacentChars.append(charBottomRight);
-        } catch (Exception _) {
-        }
-
-        System.out.println(adjacentChars);
-
-        for (String ac : adjacentChars.toString().split("")) {
-            if (specialChars.contains(ac)) {
-                return true;
-            }
-        }
-
-        return false;
-    }
 
     public static void main(String[] args) {
-        GearRatios g = new GearRatios("day03/example.txt");
-        int partsSum = g.getSumOfPartNumbers();
+        String engineSchematic = FileUtils.readFileToString("day03/example.txt");
 
-        System.out.println("Sum of part numbers: " + partsSum);
+        GearRatios gearRatios = new GearRatios(engineSchematic);
+
     }
 }
